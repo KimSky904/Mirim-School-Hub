d32629b02bf7adb46e12c3655979a14dd831da44
KimHaNeul <s2025@e-mirim.hs.kr>
2021-06-13 20:47:38 +0900

    하샤드 수 / 휴대폰 번호 가리기

---
+++ Programmers/Harshad_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(int x) {
+    bool answer = false;
+
+    int copy_x = x;
+    int sum = 0;
+
+    while (copy_x > 0) {
+        sum += copy_x % 10;
+        copy_x /= 10;
+    }
+
+    if (x % sum == 0) answer = true;
+
+    return answer;
+}
