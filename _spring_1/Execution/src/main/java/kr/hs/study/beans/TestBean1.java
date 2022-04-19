2f0b580aae2778a495505957a37c12e29ca6f528
KimSky904 <s2025@e-mirim.hs.kr>
2022-04-19 12:17:59 +0900

    [0419] AOP 옵션 적용, Execution 명시자

---
+++ _spring_1/Execution/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,25 @@
+package kr.hs.study.beans;
+
+public class TestBean1 {
+	public void method1() {
+		System.out.println("method1 호출");
+	}
+	public void method1(int a) {
+		System.out.println("int method1 호출, "+a);
+	}
+	public void method1(String a) {
+		System.out.println("String method1 호출, "+a);
+	}
+	public void method1(int a,int b) {
+		System.out.println("int 2개 method1 호출, "+a+" "+b);
+	}
+	public void method1(int a,String b) {
+		System.out.println("int, String method1 호출, "+a+" "+b);
+	}
+	public void method1(String a,String b) {
+		System.out.println("String, String method1 호출, "+a+" "+b);
+	}
+	public void method2() {
+		System.out.println("method2 호출");
+	}
+}
