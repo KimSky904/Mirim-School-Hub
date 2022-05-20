effbd87f7fd7745c23c1668a05181d44d08d52e9
KimSky904 <s2025@e-mirim.hs.kr>
2022-05-20 09:48:12 +0900

    #2 List클래스 사용법
    Java와 C# List의 차이, 라이브러리 활용

--- CSClass2022/Program.cs
+++ CSClass2022/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSClass2022
 {
@@ -17,9 +14,28 @@ namespace CSClass2022
 
 
             Random random = new Random();
-            Console.WriteLine(random.Next(10, 100)); //정수 반환
+            Console.WriteLine(random.Next(10, 100)); //정수 반환(10 ~ 99)
 
 
+            /*List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);*/
+
+            // 선언과 동시에 값 삽입하기
+            List<int> list = new List<int>() { 1,2,3,4,5,6 };
+
+            //remove 활용
+            list.Remove(50);
+
+
+
+
+            foreach (var i in list)
+            {
+                Console.WriteLine("Count : "+list.Count + "item : "+i);
+            }
 
 
         }
