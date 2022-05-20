77c87259a963d8ce69f1a5334ec4a62043134930
KimSky904 <s2025@e-mirim.hs.kr>
2022-05-20 11:18:07 +0900

    #4 클래스 인스턴스 변수와 클래스 변수
    C#은 편하게 인스턴스 변수 초기화가 가능함

---
+++ CSClass2022/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace CSClass2022
+{
+    class Product
+    {
+
+        public static string MADEIN = "Korea";
+
+        /// <summary>
+        ///  상품 가격
+        /// </summary>
+        public int price = 1000;
+
+        /// <summary>
+        /// 상품 이름
+        /// </summary>
+        public string name = "무명";
+    }
+}
