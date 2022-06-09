5b4a43163598099d9b948eefd748b6ce670d9d67
KimSky904 <s2025@e-mirim.hs.kr>
2022-06-09 13:12:30 +0900

    Label, Button, Menu 속성 알아보기

--- StopWatch/Form1.cs
+++ StopWatch/Form1.cs
@@ -17,9 +17,38 @@ namespace StopWatch
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = "더눌러봥";
+
+
+
+            //sender는 어떤 버튼이 눌렸는지의 정보가 들어온다.
+            ilStatus.Text += "+";
+            tbSataus.Text += "+";
+
+            if (Timer1.Enabled = false)
+            {
+                Timer.Enabled = true;
+                elapsedTime = 0;
+                ilStatus.Text += " ";
+                tbSataus.Text += " ";
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            ilStatus.Text += elapsedTime+"초 경과";
+            tbSataus.Text += "+";
+            toolStatusProgressBar.Value = elapsedTime;
+        }
+        private int elapsedTime = 0;
     }
 }
