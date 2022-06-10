5b0abfa23357313c42ac7ebf0f47ef5aaffeb560
KimSky904 <s2025@e-mirim.hs.kr>
2022-06-10 11:15:22 +0900

    #20 다형성 - Animal

--- CSClass2022/Program.cs
+++ CSClass2022/Program.cs
@@ -161,20 +161,14 @@ namespace CSClass2022
 
 
 
-            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
-            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
-
-            foreach(var item in Dogs)
+            //List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            //List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+            List<Animal> animals = new List<Animal>() { new Dog(), new Dog(), new Dog(), new Cat(), new Cat(), new Cat() };
+            foreach(var item in animals)
             {
                 item.Eat();
                 item.Sleep();
-                item.Bark();
-            }
-            foreach (var item in Cats)
-            {
-                item.Eat();
-                item.Sleep();
-                item.Meow();
+                ((Dog)item).Bark();
             }
 
         }
