2d534abdf223c79f00f69b7a3e3a098dbb2d326c
KimSky904 <s2025@e-mirim.hs.kr>
2022-06-14 15:52:31 +0900

    #22 클래스 자료형 변환 (Animal as Dog or Cat)

--- CSClass2022/Program.cs
+++ CSClass2022/Program.cs
@@ -188,6 +188,17 @@ namespace CSClass2022
                 }
             }
 
+            foreach (var item in animals)
+            {
+                item.Eat();
+                item.Sleep();
+                var dog = item as Dog;
+                if(dog!=null) dog.Bark();
+
+                var cat = item as Cat;
+                if(cat!=null) cat.Meow();
+            }
+
         }
     }
 }
