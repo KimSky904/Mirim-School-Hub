a840e679e54b29f96fc7f1e5f4a3990e3891bbef
KimSky904 <s2025@e-mirim.hs.kr>
2022-08-30 21:07:01 +0900

    #3 out 키워드 실습

--- Program.cs
+++ Program.cs
@@ -22,6 +22,32 @@ namespace CSClass2
             Console.Write("오늘의 점심 메뉴는 "+p[2]+" 입니다.");
             p[2] = "단무지";
             Console.Write("오늘의 점심 메뉴는 " + p[2] + " 입니다.");
+
+
+            Console.WriteLine("숫자 입력 : ");
+            //int output;
+            bool result = int.TryParse(Console.ReadLine(), out int output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자" + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+
+
+
+            int x = 0, y = 0, vx = 1, vy = 1;
+            Console.WriteLine("현재 좌표: (" + x + "," + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표: (" + x + "," + y + ")");
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
